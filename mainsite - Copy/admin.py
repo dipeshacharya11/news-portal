40129fb1c563565c5428b095d0d12005b9555383
User <user@example.com>
2025-07-16 16:55:25 +0545

    Project cleanup: Removed test files, sample data, backups and copy folders

--- mainsite - Copy/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import SiteSettings, HomePageSettings, SocialSetting
-
-
-admin.site.register(SiteSettings)
-admin.site.register(HomePageSettings)
-admin.site.register(SocialSetting)
-
